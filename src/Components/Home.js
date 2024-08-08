394ec86bd3ac0b346ff14382e82b0198111316f4
Kaleb A Butler <kaleb.butler2010@gmail.com>
2024-08-07 20:47:52 -0500

    added picture

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
 	
 		return(
-		<h2>Welcome to Flight Plans</h2>
+			<><h2>Welcome to Flight Plans</h2><image src="https://c8.alamy.com/comp/G15KR2/commercial-passenger-plane-flying-above-clouds-G15KR2.jpg" alt="plane flying through the clouds"></image></>
 	)
 }
 
